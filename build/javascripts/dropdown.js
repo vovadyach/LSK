086d95f3a2aeb9d4fab982c68694b25631c24119
vd <vd@norse.digital>
2016-04-27 18:05:22 +0300

    added popup animation and fixes some bugs

--- build/javascripts/dropdown.js
+++ build/javascripts/dropdown.js
@@ -12,20 +12,22 @@
         var $this = $(this);
         var $parent = $this.closest(".chevron-right");
         var $menu = $parent.find(".chevron-menu");
+        var $headerDropdawnParent = $this.closest(".logo-mini");
+        var $headerDropdownMenu = $headerDropdawnParent.find(".logo-dropdown-menu");
 
         $menu.toggle({height: 'show'});
+        $headerDropdownMenu.toggle({height: 'show'});
 
     });
 
     $(document).click(function(event) {
-        if(!$(event.target).closest('.chevron-menu').length &&
-            !$(event.target).is('.chevron-menu')) {
-            if($('.chevron-menu').is(":visible")) {
-                $('.chevron-menu').hide(600);
+        if(!$(event.target).closest('.chevron-menu, .logo-dropdown-menu').length &&
+            !$(event.target).is('.chevron-menu, .logo-dropdown-menu')) {
+            if($('.chevron-menu, .logo-dropdown-menu').is(":visible")) {
+                $('.chevron-menu, .logo-dropdown-menu').hide(600);
             }
         }
     });
-
 })(jQuery);
 
 
---
+++ build/javascripts/popup.js
@@ -0,0 +1,37 @@
+/**
+ * Created by vinside on 4/27/16.
+ */
+;(function ($) {
+    var $register = $(".register-link");
+    var $login = $(".login-link");
+    var $overlay = $(".overlay");
+
+    $register.on("click", function(event) {
+        var $this = $(this);
+        var $parent = $this.closest("body");
+        var $popupMenu  = $parent.find(".popup-menu-wrapper");
+        var $formRegister = $popupMenu.find(".form-register");
+
+        $popupMenu.removeClass("hide");
+        $formRegister.removeClass("hide");
+        $overlay.removeClass("hide");
+
+    });
+
+    $login.on("click", function(event) {
+        var $this = $(this);
+        var $parent = $this.closest("body");
+        var $popupMenu  = $parent.find(".popup-menu-wrapper");
+        var $formRegister = $popupMenu.find(".form-login");
+
+        $popupMenu.removeClass("hide");
+        $formRegister.removeClass("hide");
+        $overlay.removeClass("hide");
+    });
+
+    $overlay.on("click", function(){
+        $(".popup-menu-wrapper").addClass("hide");
+
+    });
+
+})(jQuery);
